eea1fd0efbd7cc7fdc476ef27aa0a58ec13b55d2
NiButera <nikolasxp683@gmail.com>
2023-03-19 01:30:04 -0300

    Fiz um programa exibir a letra da musica 'Whine Up' colorido

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Whine Up - Kat Deluna");
+
+Console.WriteLine();
+
+Thread.Sleep(1000);
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(@"Cuz I'm feeling your vibing
+I'm riding high is exotic
+And I want you, I want you here
+Pull me closer and closer and
+Hold me tight to your body
+I wanna feel you, I wanna feel you near");
+
+Console.WriteLine();
+
+Thread.Sleep(1000);
+Console.ForegroundColor = ConsoleColor.Blue; 
+Console.WriteLine(@"Whine up, whine up, whine up, oh yeah!
+Whine up, whine up, whine up, oh yeah!
+Whine up, whine up, whine up, oh yeah!
+Whine up, whine up, whine up, oh yeah!");
+
+Console.WriteLine();
